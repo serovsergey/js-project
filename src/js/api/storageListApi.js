6c6fb24ee941fb9a3e72fe1ef395b93ee43c58d4
Serov Sergey <serovsergey79@gmail.com>
2022-08-24 19:09:51 +0300

    serov2

--- src/js/api/storageListApi.js
+++ src/js/api/storageListApi.js
@@ -4,17 +4,57 @@ export default class StorageListApi {
     this.perPage = perPage;
   }
 
+  addToList(value) {
+    console.log(value)
+    const list = this.getList();
+    if (list) {
+      const length = list.push(value);
+      localStorage.setItem(this.key, JSON.stringify(list));
+      return length;
+    }
+  }
+
+  removeFromList(id) {
+    const list = this.getList();
+    if (list) {
+      const idx = list.findIndex(el => el.id === Number(id));
+      if (!~idx) {
+        console.error("Something gone wrong, guys!");
+        return false;
+      }
+      list.splice(idx);
+      localStorage.setItem(this.key, JSON.stringify(list));
+      return true;
+    }
+    return false;
+  }
+
+  inList(id) {
+    const list = this.getList();
+    if (list) {
+      return list.some(el => el.id === Number(id));
+    }
+    return false;
+  }
+
+  getList() {
+    const stored = localStorage.getItem(this.key);
+    let list = null;
+    try {
+      list = JSON.parse(stored);
+    }
+    catch (e) { console.error(e.message) }
+    return list;
+  }
+
   fetchNext(page = -1) {
+    const list = this.getList();
+    if (!list) return null;
     if (~page)
       this.page = page;
     else
       this.page += 1;
-    const stored = localStorage.getItem(this.key);
-    let data = null;
-    try {
-      data = JSON.parse(stored);
-    }
-    catch { }
-    if (!data) return null;
+    const from = (page - 1) * this.perPage;
+    return list.slice(from, from + this.perPage);
   }
 }
